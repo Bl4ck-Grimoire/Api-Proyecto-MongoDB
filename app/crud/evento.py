4eaef655fbe49e8ceae1cdb38e3771632d5f43b6
Bl4ck-Grimoire <juan_dav.lasso@uao.edu.co>
2025-11-05 00:00:28 -0500

    Proyecto

--- app/crud/evento.py
+++ app/crud/evento.py
@@ -1,3 +1,8 @@
 from fastapi import HTTPException,status
 from beanie import PydanticObjectId
 from typing import List, Optional
+
+async def crear_evento(evento_model, EventoModel):
+    evento = EventoModel(**evento_model.dict())
+    await evento.insert()
+    return evento
--- app/schemas/evento.py
+++ app/schemas/evento.py
@@ -1,3 +1,66 @@
 from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional, List
 from datetime import date
+from app.models.lugaresEvento import lugaresEvento
+from app.models.organizacionesExternas import OrganizacionExterna
+from app.models.usuariosOrganizadores import UsuarioOrganizador
+
+class EventoBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    nombre: str
+    tipo_evento: str
+    descripcion_evento: str
+    fecha_evento: date
+    hora_inicio: str
+    hora_fin: str
+    estado_evento: str
+    orgaznizado_por: str
+    tipo_aval: str
+    lugares_evento: Optional[List[lugaresEvento]] = []
+    organizaciones_externas: Optional[List[OrganizacionExterna]] = []
+    usuarios_organizadores: Optional[List[UsuarioOrganizador]] = []
+    evento_aval_url: str
+
+class CrearEvento(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    nombre: str
+    tipo_evento: str
+    descripcion_evento: str
+    fecha_evento: date
+    hora_inicio: str
+    hora_fin: str
+    estado_evento: str
+    orgaznizado_por: str
+    tipo_aval: str
+    lugares_evento: Optional[List[lugaresEvento]] = []
+    organizaciones_externas: Optional[List[OrganizacionExterna]] = []
+    usuarios_organizadores: Optional[List[UsuarioOrganizador]] = []
+    evento_aval_url: str
+
+class ActualizarEvento(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    nombre: Optional[str] = None
+    tipo_evento: Optional[str] = None
+    descripcion_evento: Optional[str] = None
+    fecha_evento: Optional[date] = None
+    hora_inicio: Optional[str] = None
+    hora_fin: Optional[str] = None
+    estado_evento: Optional[str] = None
+    orgaznizado_por: Optional[str] = None
+    tipo_aval: Optional[str] = None
+    lugares_evento: Optional[List[lugaresEvento]] = None
+    organizaciones_externas: Optional[List[OrganizacionExterna]] = None
+    usuarios_organizadores: Optional[List[UsuarioOrganizador]] = None
+    evento_aval_url: Optional[str] = None
+
+class Evento(CrearEvento):
+    id: str = Field(..., description="ID único del evento")
+    
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
