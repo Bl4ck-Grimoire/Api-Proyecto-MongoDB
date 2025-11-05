09ed414b94c2643236efddca6aab193148745b50
Bl4ck-Grimoire <juan_dav.lasso@uao.edu.co>
2025-11-04 23:00:14 -0500

    ola desde gitju

--- app/models/evento.py
+++ app/models/evento.py
@@ -5,6 +5,18 @@ from datetime import date
 class EventoModel(Document):
     id: Optional[PydanticObjectId] = None
     nombre: str
+    tipo_evento: str
+    descripcion_evento: str
+    fecha_evento: date
+    hora_inicio: str
+    hora_fin: str
+    estado_evento: str
+    orgaznizado_por: str
+    tipo_aval: str
+    lugares_evento: Optional[List[lugaresEvento]] = []
+    organizaciones_externas: Optional[List[organizacionExterna]] = []
+    usuarios_organizadores: Optional[List[usuariosOrganizador]] = []
+    evento_aval_url: str
 
     class Settings:
         name = "eventos"
