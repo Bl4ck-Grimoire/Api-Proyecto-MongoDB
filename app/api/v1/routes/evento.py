f7b4ddd0c7a6bdd53195420c39b22e0710fc297d
Bl4ck-Grimoire <juan_dav.lasso@uao.edu.co>
2025-11-09 15:39:21 -0500

    Proyecto

--- app/api/v1/routes/evento.py
+++ app/api/v1/routes/evento.py
@@ -5,24 +5,24 @@ from app.schemas.evento import EventoBase, CrearEvento, ActualizarEvento, Evento
 
 router = APIRouter()
 
-@router.post("/", response_model=Evento, status_code=status.HTTP_201_CREATED)
+@router.post("/", summary="Crear Evento", response_model=Evento, status_code=status.HTTP_201_CREATED)
 async def crearEvento(evento: CrearEvento):
     return await crud.crear_evento(evento)
 
-@router.put("/{evento_id}", response_model=Evento, status_code=status.HTTP_200_OK)
+@router.put("/{evento_id}", summary="Actualizar Evento", response_model=Evento, status_code=status.HTTP_200_OK)
 async def actualizarEvento(
     evento_id: str = Path(..., description="ID del evento a actualizar"), evento: ActualizarEvento = ...):
     return await crud.actualizar_evento(evento_id, evento)
 
-@router.get("/{evento_id}", response_model=Evento, status_code=status.HTTP_200_OK)
+@router.get("/{evento_id}", summary="Obtener Evento",  response_model=Evento, status_code=status.HTTP_200_OK)
 async def obtenerEvento(evento_id: str = Path(..., description="ID del evento a obtener")):
     return await crud.obtener_evento_por_id(evento_id)
 
-@router.get("/", response_model=List[Evento], status_code=status.HTTP_200_OK)
+@router.get("/", summary="Obtener todos los Eventos", response_model=List[Evento], status_code=status.HTTP_200_OK)
 async def listarEventos():
     return await crud.obtener_eventos()
 
-@router.delete("/{evento_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{evento_id}", summary="Eliminar Evento", status_code=status.HTTP_204_NO_CONTENT)
 async def eliminarEvento(evento_id: str = Path(..., description="ID del evento a eliminar")):
     await crud.eliminar_evento(evento_id)
     return None
--- app/api/v1/routes/usuarios.py
+++ app/api/v1/routes/usuarios.py
@@ -5,24 +5,24 @@ from app.schemas.usuarios import UsuarioBase, CrearUsuario, ActualizarUsuario, U
 
 router = APIRouter()
 
-@router.post("/", response_model=Usuario, status_code=status.HTTP_201_CREATED)
+@router.post("/", summary="Crear Usuario", response_model=Usuario, status_code=status.HTTP_201_CREATED)
 async def crear_usuario(usuario: CrearUsuario):
     return await crud.crear_usuario(usuario)
 
-@router.put("/{usuario_id}", response_model=Usuario, status_code=status.HTTP_200_OK)
+@router.put("/{usuario_id}", summary="Actualizar Usuario", response_model=Usuario, status_code=status.HTTP_200_OK)
 async def actualizar_usuario(
     usuario_id: str = Path(..., description="ID del usuario a actualizar"), usuario: ActualizarUsuario = ...):
     return await crud.actualizar_usuario(usuario_id, usuario)
 
-@router.get("/{usuario_id}", response_model=Usuario, status_code=status.HTTP_200_OK)
+@router.get("/{usuario_id}", summary="Obtener Usuario", response_model=Usuario, status_code=status.HTTP_200_OK)
 async def obtener_usuario(usuario_id: str = Path(..., description="ID del usuario a obtener")):
     return await crud.obtener_usuario_por_id(usuario_id)
 
-@router.get("/", response_model=List[Usuario], status_code=status.HTTP_200_OK)
+@router.get("/", summary="Obetener todos los Usuarios", response_model=List[Usuario], status_code=status.HTTP_200_OK)
 async def listar_usuarios():
     return await crud.obtener_usuarios()
 
-@router.delete("/{usuario_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{usuario_id}", summary="Eliminar Usuario", status_code=status.HTTP_204_NO_CONTENT)
 async def eliminar_usuario(usuario_id: str = Path(..., description="ID del usuario a eliminar")):
     await crud.eliminar_usuario(usuario_id)
     return None
